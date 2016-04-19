94612c38402638891beaac302ad1481c6934bc7b
dbosin <diego.bosin@mercadolibre.com>
2016-04-19 01:27:05 -0300

    first commit

---
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -0,0 +1,38 @@
+package com.teamtreehouse.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FunFactsActivity extends AppCompatActivity {
+    private FactBook mFactBook = new FactBook();
+    //Declare our view variable
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        //Assign the Views from the layout file to the corresponding variables
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+
+                // Update the screen with your dynamic fact
+                mFactTextView.setText(fact);
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
+
+
+    }
+}
